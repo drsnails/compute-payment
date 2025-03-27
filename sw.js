7fa9d403a84592d1f3915d34a09bcca362afa41b
Stav Yaar Bar <anistu@gmail.com>
2025-03-27 15:48:00 +0200

    new version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log('Service Worker Registered!');
 
-let CACHE_NAME = 'lawyers-fee-cache-v11';  // Update the version when you want to push new content
+let CACHE_NAME = 'lawyers-fee-cache-v15';  // Update the version when you want to push new content
 
 const urlsToCache = [
     'https://drsnails.github.io/compute-payment/',
